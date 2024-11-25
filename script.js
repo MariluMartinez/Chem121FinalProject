fcbfedf8429abd942b07d0e2f90e51e782d55e56
Marilu Martinez <112132884+MariluMartinez@users.noreply.github.com>
2024-11-25 01:05:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,22 @@
 
             // Change the text in the info box based on the clicked word
             var infoText = document.getElementById('info-text');
+            var infoImage = document.getElementById('info-image');
+            
             if (word === 'neurotransmitter') {
-                infoText.textContent = "This is the additional information about the 'neurotransmitter' word.";
-            } else if (word === 'important') {
-                infoText.textContent = "This is the additional information about the 'important' word.";
-            } else {
+                infoText.textContent = "Neurotransmitters are the body's chemical messengers. It helps transmit signals in the brain and the nervous system. Neurotransmitters are located in the part of the neuron called the axon terminal. They are stored in sacs called synaptic vesicles. An electrical signal is sent to the axon terminal, telling the neurotransmitter to move to the nerve cell membrane and is released from the axon terminal to a fluid-filled space between one nerve cell and the target cell. From this space, the neurotransmitter carries the message across. Each type of neurotransmitter lands on and binds to a specific receptor. It's a key-and-lock system.";
+                infoImage.src = "neurotransmitters.jpg";
+                infoImage.style.display = 'block';
+            } else if (word === 'anticholinergic') {
+                infoText.textContent = "Anticholinergic medicines are a class of drugs that block acetylcholine from binding to its muscarinic receptors. These medicines are used to treat a wide range of conditions if acetylcholine's action is problematic or excessive. These medicines can either reduce secretions, relax smooth muscles, or increase heart rate. Most medicines have a similar structure to acetylcholine, letting it bind to the muscarinic receptors. They all have different compound structures containing carbon, hydrogen, nitrogen, and oxygen.";
+                infoImage.style.display = 'none'; 
+            } else if (word === 'side chains') {
+                infoText.textContent = 'Side chains refers to a group of atoms attached to the main structure of a molecule. Side chains vary in size, structure, and properties, and play an important role in the chemical behavior, function, and properties of the molecule. '; 
+                infoImage.style.display = 'none'; 
+            } 
+            else {
                 infoText.textContent = "This is the default additional information.";
+                infoImage.style.display = 'none';
             }
 
             // Toggle the visibility of the info box
